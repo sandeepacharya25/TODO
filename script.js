52ebb8addb1da1c2a3484d2f2cb6b10b88782bcc
sandeepacharya25 <acharyasandeep061@gmail.com>
2021-12-08 15:57:43 +0545

    todo

---
+++ script.js
@@ -0,0 +1,37 @@
+const input =document.querySelector('input');
+const btn =document.querySelector('.addTask > button');
+
+btn.addEventListener('click',addList);
+
+function addList(e){
+    const notCompleted =document.querySelector('.notCompleted');
+    const Completed =document.querySelector('.Completed');
+
+    const newLi=document.createElement('li');
+    const checkBtn=document.createElement('button');
+    const delBtn=document.createElement('button');
+
+    checkBtn.innerHTML='<i class="material-icons">check</i>';
+    delBtn.innerHTML='<i class="material-icons">delete</i>';
+
+    if(input.value!==''){
+        newLi.textContent=input.value;
+        input.value='';
+        notCompleted.appendChild(newLi);
+        newLi.appendChild(checkBtn);
+        newLi.appendChild(delBtn);
+    }
+
+    checkBtn.addEventListener('click',function(){
+        const parent=this.parentNode;
+        parent.remove();
+        Completed.appendChild(parent);
+        checkBtn.style.display='none';
+        });
+
+        delBtn.addEventListener('click',function(){
+            const parent=this.parentNode;
+            parent.remove();
+            })
+
+}
